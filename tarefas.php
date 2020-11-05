aad45e4301e2194c3915d67f000c082f80670a72
julio6767 <57772285+julio6767@users.noreply.github.com>
2020-11-05 12:29:02 -0300

    Add files via upload

---
+++ tarefas.php
@@ -0,0 +1,43 @@
+<html>
+
+
+
+
+
+				<head>
+						<title>Gerenciador	de	Tarefas</title>
+				</head>
+
+
+
+				<body>
+
+					<h1>Gerenciador	de	Tarefas</h1>
+
+
+
+						<form>
+								<fieldset>
+												<legend>Nova	tarefa</legend>
+												<label>
+                                                		Tarefa:
+																<input	type="text" name="nome"	/>
+												</label>
+												<input	type="submit" value="Cadastrar"	/>
+								</fieldset>
+				        </form>
+
+                <?php
+				if	(array_key_exists('nome',	$_GET))	{
+								echo "Nome	informado:	"	.	$_GET['nome'];
+				}
+                
+                ?>
+
+
+
+
+				</body>
+
+
+				</html>
