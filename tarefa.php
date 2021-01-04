1236a0f28de4d0d5af7b36313f2221aa6df94f9d
julio6767 <juliocesarsoares102@gmail.com>
2021-01-04 17:34:43 -0300

    validaçao anexo

--- tarefa.php
+++ tarefa.php
@@ -5,6 +5,43 @@ $tem_erros	=	false;
 $erros_validacao	=	[];
 if	(tem_post())	{
 				//	upload	dos	anexos
+			$tarefa_id =  $_POST['tarefa_id'];
+
+		if (! array_key_exists('anexo', $_FILES))	{
+				$tem_erros = true;
+				$erros_validacao ['anexo'] = 
+				'Voce deve selecionar um arquivo para anexar' ;
+		}
+	else {
+		if (tratar_anexo($_FILES['anexo']))  {
+			$nome = $_FILES['anexo']['name'];
+			$anexo = [
+
+				'$tarefa_id' => $tarefa_id,
+				'nome'=> substr ($nome, 0, -4);
+				'arquivo'=> $nome, 
+			];
+
+			/*
+				"substr()	,	que	pega	trechos de	uma	string." 
+
+
+				na variável		$nome	,	o	começo	do	trecho	do	texto	que	queremos	extrair,
+				que	é	a	posição	zero	e,	por	último,	até	onde	queremos	cortar	o	texto,
+				que	é	até	faltar	4	caracteres	para	o	final	do	nome.	Assim,	se	o	nome
+				for	algo	como		relatorio.pdf	,	o		.pdf		será	removido	e	ficaremos
+				apenas	com	o	texto		relatorio	 */
+
+			else {
+				$tem_erros = true;
+				$erros_validacao ['anexo'] = 'Envie anexos nos formatos ZIP ou PDF';
+			}
+		}
+
+		if (! $tem_erros) {
+			gravar_anexo = ($conexao, $anexo);
+		}
+	}
 }
 
 
